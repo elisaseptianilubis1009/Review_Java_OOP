0975d7faa4cd704d1ef25d54e0fabb48feee46a8
elisaseptiani1009 <elisaseptianlubis1009@gmail.com>
2021-10-28 10:22:23 +0700

    init

---
+++ src/review_java_oop/Review_Java_OOP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package review_java_oop;
+
+/**
+ *
+ * @author admin
+ */
+public class Review_Java_OOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
